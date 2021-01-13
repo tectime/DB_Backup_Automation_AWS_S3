caeb4b781447c02c0abb2438e0755c284bf43045
tectime <robsonmessias80@gmail.com>
2021-01-13 11:39:07 +0100

    Add files via upload

---
+++ tests/test_storage.py
@@ -0,0 +1,30 @@
+import tempfile
+import pytest
+
+from pgbackup import storage
+
+
+@pytest.fixture
+def infile():
+    infile = tempfile.TemporaryFile('r+b')
+    infile.write(b"Testing")
+    infile.seek(0)
+    return infile
+
+def test_storing_file_locally(infile):
+    """
+    Writes content from one file-like to another
+    """
+    outfile = tempfile.NamedTemporaryFile(delete=False)
+    storage.local(infile, outfile)
+    with open(outfile.name, 'rb') as f:
+        assert f.read() == b'Testing'
+
+def test_storing_file_on_s3(mocker, infile):
+    """
+    Writes content from one file-like to S3
+    """
+    client = mocker.Mock()
+    storage.s3(client, infile, "bucket", "file-name")
+    client.upload_fileobj.assert_called_with(infile, "bucket", "file-name")
+
